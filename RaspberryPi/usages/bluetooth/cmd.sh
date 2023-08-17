3f88a72532371970ee3c9da430e22deae936ca51
Alegal200 <alex.gallez11@gmail.com>
2023-08-17 17:14:34 +0200

    dirty

---
+++ RaspberryPi/usages/bluetooth/cmd.sh
@@ -0,0 +1,7 @@
+bluetoothhctl
+power on
+discoverable on
+scan on
+pairable on
+scan off
+connect 4C:EA:AE:B3:D9:CC
---
+++ RaspberryPi/usages/scripts/lora/t.cpp
@@ -0,0 +1,9 @@
+@ -0,0 +1,9 @@
+// Your First C++ Program
+ // g++ -o test tesst.cpp  
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0 ;
+}
