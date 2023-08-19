123d81ac4f597a7bc6edf0050d437093c511a4c2
Alegal200 <alex.gallez11@gmail.com>
2023-08-19 17:28:17 +0200

    led & relay

--- RaspberryPi/usages/scripts/light.py
+++ RaspberryPi/usages/scripts/light.py
@@ -1,6 +1,10 @@
+
 #!/usr/bin/env python
 #
-# GrovePi Example for using the Grove Relay (http://www.seeedstudio.com/wiki/Grove_-_Relay)
+# GrovePi Example for using the Grove Light Sensor and the LED together to turn the LED On and OFF if the background light is greater than a threshold.
+# Modules:
+# 	http://www.seeedstudio.com/wiki/Grove_-_Light_Sensor
+# 	http://www.seeedstudio.com/wiki/Grove_-_LED_Socket_Kit
 #
 # The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
 #
@@ -32,30 +36,42 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
-# NOTE: Relay is normally open. LED will illuminate when closed and you will hear a definitive click sound
+
 import time
 import grovepi
 
-# Connect the Grove Relay to digital port D4
+# Connect the Grove Light Sensor to analog port A0
 # SIG,NC,VCC,GND
-relay = 5
+light_sensor = 0
 
-grovepi.pinMode(relay,"OUTPUT")
+# Connect the LED to digital port D4
+# SIG,NC,VCC,GND
+led = 6
+
+# Turn on LED once sensor exceeds threshold resistance
+threshold = 10
+
+grovepi.pinMode(light_sensor,"INPUT")
+grovepi.pinMode(led,"OUTPUT")
 
 while True:
     try:
-        # switch on for 5 seconds
-        grovepi.digitalWrite(relay,1)
-        print ("on")
-        time.sleep(5)
+        # Get sensor value
+        sensor_value = grovepi.analogRead(light_sensor)
 
-        # switch off for 5 seconds
-        grovepi.digitalWrite(relay,0)
-        print ("off")
-        time.sleep(5)
+        # Calculate resistance of sensor in K
+        resistance = (float)(1023 - sensor_value) * 10 / sensor_value
 
-    except KeyboardInterrupt:
-        grovepi.digitalWrite(relay,0)
-        break
+        if resistance > threshold:
+            # Send HIGH to switch on LED
+            grovepi.digitalWrite(led,1)
+        else:
+            # Send LOW to switch off LED
+            grovepi.digitalWrite(led,0)
+
+        print("{light:%d" %(sensor_value) , end="")
+        print("}")
+        time.sleep(.5)
+
     except IOError:
         print ("Error")
