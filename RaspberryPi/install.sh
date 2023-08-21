35adc5fdf68332b6202dcdd59482a68a744df793
Alegal200 <alex.gallez11@gmail.com>
2023-08-21 20:31:05 +0200

    esp8266

--- RaspberryPi/install.sh
+++ RaspberryPi/install.sh
@@ -17,17 +17,23 @@ curl -kL dexterindustries.com/update_grovepi 5 | bash
 #python grove_led_blink.py ##check if working with LED connected to D4 on GrovePi
 
 #
-#
 #       LORA /GPS HAT v 1.4 
-#
-#
-
 cd /tmp
 wget https://github.com/WiringPi/WiringPi/releases/download/2.61-1/wiringpi-2.61-1-armhf.deb
 sudo dpkg -i wiringpi-2.61-1-armhf.deb
+# vnc 
+sudo apt-get install tightvncserver -y
+# tightvncserver
+# todo : add  vncserver :1 when rpi is starting
 
-
-
-
-
-
+#
+#   MQTT
+#
+sudo apt install -y mosquitto
+sudo apt install -y mosquitto-clients -y 
+#
+#
+#test.mosquitto.org
+#mosquitto_pub -h test.mosquitto.org -t DIY-bike/sensor/temp -m 22.5
+#mosquitto_sub -h test.mosquitto.org -t DIY-bike/sensor/temp
+#sudo systemctl enable mosquitto.service
