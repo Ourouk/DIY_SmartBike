083a2e1090c13333d7fa34fa0e00d7f435e0d2d6
Andrea Spelgatti <spelgattiandrea@gmail.com>
2023-08-26 16:14:38 +0200

    Add an easy to run bash script to start servers

---
+++ rocky-linux/docker/launch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Specify the name of the service you want to run
+SERVICE_NAME="omgwtfssl"
+
+# Specify the path to the file you want to check
+FILE_PATH="ca-key.pem"
+
+#Check if user run this script as root
+if [ $UID != 0 ]; then
+    echo "Please run this script with sudo:"
+    sudo "$0" "$@"
+    exit 1
+fi
+
+# Check if the file exists
+if [ -f "$FILE_PATH" ]; then
+    echo "File exists. Starting the service..."
+    docker-compose up -d "$SERVICE_NAME"
+    
+    # Wait for the service to finish launching
+    docker-compose logs -f "$SERVICE_NAME"
+    
+    echo "Service has finished launching."
+
+    echo "Fixing read/write permission error"
+
+    chmod 777 -R  certs/
+else
+    echo "File does not exist. Skipping the service start."
+fi
+
+docker compose up -d mosquitto mongodb nodered
+
+
